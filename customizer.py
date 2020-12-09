6e434b3ac12d9fa73f7cd7205f0301df98697ad5
galva041 <galva041@ucr.edu>
2020-12-09 01:29:47 -0800

    Customizer finished, squashed bugs

--- customizer.py
+++ customizer.py
@@ -1,40 +1,168 @@
-from pet import *
+from pet import Pet
+from menu import *
 
-class PetCustomizer(Pet):
+class Abstract_Pet(Pet):                    
     def customizer(self, pet):
-        self.pet = pet
-    def put_on_pants(self):
         pass
-    def put_on_shirt(self):
+    def get_pants(self):
         pass
-    def put_on_hat(self):
+    def set_pants(self, type):
         pass
-    def getFit(self):
-        p = False
-        s = False
-        h = False
-        if self.pants == True:
-            p = True
-        if self.shirt == True:
-            s = True
-        if self.hat == True:
-            h = True
-        return p,s,h
-class Pants(PetCustomizer):
-    def __init__(self,pants):
-        self.pants = pants
-    def put_on_pants(self):
-        return self.pants.put_on_pants()
+    def get_shirt(self):
+        pass
+    def get_hat(self):
+        pass
+    def set_shirt(self, type):
+        pass
+    def set_hat(self, type):
+        pass
     
-class Shirt(PetCustomizer):
-    def __init__(self, Pet):
-        PetCustomizer.__init__(Pet)
-    def customizer(self):
-        self.shirt = True
+class Concrete_Pet(Abstract_Pet):
+        def customizer(self, Pet):
+            return Pet()
+        def get_pants(self):
+            return "Pants: "
+        def set_pants(self):
+            return "None"
+        def get_shirt(self):
+            return "Shirt: "
+        def set_shirt(self):
+            return "None"
+        def get_hat(self):
+            return "Hat: "
+        def set_hat(self):
+            return "None"
 
-class Hat(PetCustomizer):
-    def __init__(self, Pet):
-        PetCustomizer.__init__(Pet)
-    def customizer(self):
-        self.hat = True
-        
+class PetDecorator(Pet):                                     #abstarct decorator 
+    def __init__(self,decorated_pet):
+        self.decorated_pet = decorated_pet
+    def get_pants(self):
+        return self.decorated_pet.get_pants()
+    def get_shirt(self):
+        return self.decorated_pet.get_shirt()
+    def get_hat(self):
+        return self.decorated_pet.get_hat()
+
+class Jeans(PetDecorator):                                  # pants decorator 
+    def __init__(self,decorated_pet):
+        PetDecorator.__init__(self,decorated_pet)
+    def set_pants(self):
+        return "Jeans 👖"
+    def get_pants(self):
+       return "Pants: " + self.set_pants() 
+
+class Skirt(PetDecorator):
+    def __init__(self,decorated_pet):
+        PetDecorator.__init__(self,decorated_pet)
+    def set_pants(self):
+        return "Skirt 👗"
+    def get_pants(self):
+       return "Pants: " + self.set_pants() 
+
+class Shorts(PetDecorator):
+    def __init__(self,decorated_pet):
+        PetDecorator.__init__(self,decorated_pet)
+    def set_pants(self):
+        return "Shorts 🩳"
+    def get_pants(self):
+       return "Pants: " + self.set_pants() 
+
+class TShirt(PetDecorator):
+    def __init__(self,decorated_pet):
+        PetDecorator.__init__(self,decorated_pet)
+    def set_shirt(self):
+        return "T-Shirt 👚"
+    def get_shirt(self):
+       return "Shirt: " + self.set_shirt() 
+
+class TankTop(PetDecorator):
+    def __init__(self,decorated_pet):
+        PetDecorator.__init__(self,decorated_pet)
+    def set_shirt(self):
+        return "Tank Top 🎽"
+    def get_shirt(self):
+       return "Shirt: " + self.set_shirt() 
+
+class DressShirt(PetDecorator):
+    def __init__(self,decorated_pet):
+        PetDecorator.__init__(self,decorated_pet)
+    def set_shirt(self):
+        return "Dress Shirt 👔"
+    def get_shirt(self):
+       return "Shirt: " + self.set_shirt() 
+
+class Beanie(PetDecorator):
+    def __init__(self,decorated_pet):
+        PetDecorator.__init__(self,decorated_pet)
+    def set_hat(self):
+        return "Beanie 🧢"
+    def get_hat(self):
+       return "Hat: " + self.set_hat() 
+
+class Crown(PetDecorator):
+    def __init__(self,decorated_pet):
+        PetDecorator.__init__(self,decorated_pet)
+    def set_hat(self):
+        return "Crown 👑"
+    def get_hat(self):
+       return "Hat: " + self.set_hat() 
+
+class GradCap(PetDecorator):
+    def __init__(self,decorated_pet):
+        PetDecorator.__init__(self,decorated_pet)
+    def set_hat(self):
+        return "Graduation Cap 🎓"
+    def get_shirt(self):
+       return "Hat: " + self.set_hat() 
+
+
+
+def main(): 
+    p = Concrete_Pet()
+    change = "1"
+    while change != "5":
+        CustomMenu()
+        change = input()
+        if change == "1":               #shirt
+            shirtMenu()
+            option = input()
+            if option == "1":
+                p = TShirt(p)
+            if option == "2":
+                p = TankTop(p)
+            if option == "3":
+                p = DressShirt(p)
+            if option != "1" and option !="2" and option !="3":
+                print("\nInvalid Input! Pick a choice listed in the menu.\n")
+            print('Fit: ', '\n', p.get_pants(), '\n',  p.get_shirt(), '\n',  p.get_hat())
+        if change == "2":               #pants
+            pantsMenu()
+            option = input()
+            if option == "1":
+                p = Skirt(p)
+            if option == "2":
+                p = Jeans(p)
+            if option == "3":
+                p = Shorts(p)
+            if option != "1" and option !="2" and option !="3":
+                print("\nInvalid Input! Pick a choice listed in the menu.\n")
+            print('Fit: ', '\n', p.get_pants(), '\n',  p.get_shirt(), '\n',  p.get_hat())
+        if change == "3":               #hat
+            hatMenu()
+            option = input()
+            if option == "1":
+                p = Beanie(p)
+            if option == "2":
+                p = Crown(p)
+            if option == "3":
+                p = GradCap(p)
+            if option != "1" and option !="2" and option !="3":
+                print("\nInvalid Input! Pick a choice listed in the menu.\n")
+            print('Fit: ', '\n', p.get_pants(), '\n',  p.get_shirt(), '\n',  p.get_hat())
+        if change == "5":
+            print("Returning to menu...")
+        if change != "1" and change !="2" and change !="3" and change !="4" and change !="5":
+            print("\nInvalid Input! Pick a choice listed in the menu.\n")
+
+if __name__ == "__main__":
+    main()
--- menu.py
+++ menu.py
@@ -13,3 +13,41 @@ def Welcome():
     print("================================ \n")
     print("     Welcome to Study Mates!   \n")
     print("================================ \n")
+
+def CustomMenu():
+    print("================================ \n")
+    print(" What do you want to customize?   \n")
+    print("================================ \n")
+    print("     1: Change Shirt    \n")
+    print("     2: Change Pants \n")
+    print("     3: Change Hat    \n")
+    print("     4: Change Name    \n")
+    print("     5: Done    \n")
+    print("================================ \n")
+
+def pantsMenu():
+    print("================================ \n")
+    print("          Pant Options           \n")
+    print("================================ \n") 
+    print("     1: Skirt 👗    \n")
+    print("     2: Jeans 👖\n")
+    print("     3: Shorts 🩳\n")
+    print("================================ \n")
+ 
+def shirtMenu():
+    print("================================ \n")
+    print("          Shirt Options           \n")
+    print("================================ \n") 
+    print("     1: T-Shirt 👚    \n")
+    print("     2: Tank top 🎽 \n")
+    print("     3: Dress shirt 👔 \n")
+    print("================================ \n")
+
+def hatMenu():
+    print("================================ \n")
+    print("          Hat Options           \n")
+    print("================================ \n") 
+    print("     1: Beanie 🧢    \n")
+    print("     2: Crown 👑\n")
+    print("     3: Graduation Cap 🎓\n")
+    print("================================ \n")
